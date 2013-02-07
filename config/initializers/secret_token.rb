2c49c31fbfd4005a2ed970193daf8c58e6ff6439
Aaron Rosen <azrosen92@gmail.com>
2013-02-06 23:27:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AepiAlphaPsi::Application.config.secret_token = '8e00924c2f11fc46440a0af2b997b4f2ab2f700d932c7feaa042bcbe31184bda7938d2f7fc6a1478322d39bf010a383043719e5f9eba16e8a596c7796a0a494e'

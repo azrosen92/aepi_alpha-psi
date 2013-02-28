729ce30b0a225658eb20e0e6cc0a49c70046a1a9
Aaron Rosen <azrosen92@gmail.com>
2013-02-27 22:16:49 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 AepiAlphaPsi::Application.routes.draw do
 
-  get "users/new"
 
 	root to: 'static_pages#launch_page'	
 
 	resources :users
+	resources :sessions, only: [:new, :create, :destroy]
 
 	match '/about', to: 'static_pages#about'
 	match '/signup', to: 'users#new'
+	match '/signin', to: 'sessions#new'
+	match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
 	factory :user do
 		name "Example User"
 		email "example@user.com"
+		password "foobar"
+		password_confirmation "foobar"
 	end
 end

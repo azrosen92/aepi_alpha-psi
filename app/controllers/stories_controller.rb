3d827044ee0bb1cb3285c28056af7154996985bb
Aaron Rosen <azrosen92@gmail.com>
2013-03-18 12:18:22 -0400

    finish basic news page without styling

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,16 +1,15 @@
 class StoriesController < ApplicationController
 
 	def index
-		@stories = Story.all
-	end
-
-	def new
-		@story = Story.new
+		@stories = Story.all.reverse
+    @story = Story.new
 	end
 
 	def create
+    @story = Story.new(params[:story])
 		if @story.save
-			
+			flash[:success] = "Story saved!"
+      redirect_to stories_path
 		else
 
 		end
